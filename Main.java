ea78ff5ac9bfbf65cfb887492e5ec9e62351f6ac
nooro <georgi.dancheff@gmail.com>
2018-12-29 00:50:09 +0200

    Add the main method to can test the db connectivity

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+import UI.RegistrationForm;
+
+
+public class Main {
+	
+	public static void main( String[] args) {
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/library", "root", "");
+		
+	}
+	
+}
