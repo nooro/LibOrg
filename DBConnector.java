d4d1f854192805e82a0a4bdfd08b1e604acbc9f5
nooro <georgi.dancheff@gmail.com>
2018-12-30 00:56:30 +0200

    Create search method for searching books by title or author mixed

--- DBConnector.java
+++ DBConnector.java
@@ -17,4 +17,41 @@ public class DBConnector {
 		}
 	}
 	
+	public List<Book> search(String searchString) {
+		Statement statement;
+		String [] splittedSearchString = searchString.split(" ");
+		
+		//Build the query 
+		String query = "SELECT * FROM books";
+		if(splittedSearchString.length >= 2) {
+			for(int i = 0; i < splittedSearchString.length; i++) {
+				if( i == 0)
+					query += " WHERE title LIKE '%"+splittedSearchString[i]+"%' OR  author LIKE '%"+splittedSearchString[i]+"%'";
+				else
+					query += " AND title LIKE '%"+splittedSearchString[i]+"%' OR author LIKE '%"+splittedSearchString[i]+"%'";
+			}
+			query += ";";
+		} else {
+			query = "SELECT * FROM books WHERE title LIKE '%"+searchString+"%' OR author LIKE '%"+searchString+"%';";
+		}
+		
+		
+		try {
+			statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(query);
+			
+			List <Book> books = new LinkedList<Book>();
+			
+			while(result.next()) {
+				books.add(new Book( result.getInt("id"), result.getString("title"), result.getString("author"), result.getString("genre"), result.getInt("publishing_year"), result.getInt("pages"), result.getInt("amount")));
+			}
+			
+			return books;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 }
