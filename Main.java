d9109179a35da93327d2fa1ef7cdd169a61e0676
nooro <georgi.dancheff@gmail.com>
2018-12-30 00:56:59 +0200

    Test the search method

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 import java.util.List;
 
-import UI.RegistrationForm;
-
-
 public class Main {
 	
 	public static void main( String[] args) {
+		
 		DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/library", "root", "");
 		
+		List<Book> books = db.search("terry ");
+		
+		for(Book book : books) {
+			System.out.println(book.getTitle());
+		}
 	}
 	
 }
