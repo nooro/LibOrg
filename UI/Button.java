71a4ce421a75bbb431de2bc5b6d73ee981f62ae1
nooro <georgi.dancheff@gmail.com>
2018-12-30 04:18:53 +0200

    Created Button class to make the creation of JButtons easier

---
+++ UI/Button.java
@@ -0,0 +1,33 @@
+package UI;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JButton;
+
+public class Button {
+	
+	private JButton button;
+	Dimension size;
+	
+	public Button(String text, Font font, Color foreground, Color background) {
+		this.button = new JButton(text);
+		
+		this.button.setFont(font);
+		this.button.setBorder(null);
+		
+		this.button.setBackground(background);
+		this.button.setForeground(foreground);
+		
+		size = this.button.getPreferredSize();
+	}
+	
+	public Dimension getSize() {
+		return this.size;
+	}
+	
+	public JButton getJButton() {
+		return this.button;
+	}
+}
