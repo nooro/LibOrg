7ff0b559d33eea6a03c6780acf372c87d9a9b76c
nooro <georgi.dancheff@gmail.com>
2019-01-04 16:43:19 +0200

    Group the fonts for the log-in and registration to be in one sub font class - Forms

--- UI/Resources.java
+++ UI/Resources.java
@@ -8,31 +8,20 @@ import javax.swing.JFrame;
 public class Resources {
 	
 	public static class Fonts {
-		public static class Registration {
-			public static Font title;
-			public static Font inputFields;
-			public static Font button;
-			public static Font link; 
-			
-			public static void setUp(JFrame window) {
-				title = new Font("Tahoma", Font.BOLD, window.getWidth()/10);
-				inputFields = new Font("Tahoma", Font.ITALIC, window.getWidth()/20);
-				button = new Font("Tahoma", Font.BOLD, window.getWidth()/25);
-				link = new Font("Tahoma", Font.ITALIC, window.getWidth()/30);
-			}
-		}
 		
-		public static class LogIn {
+		public static class Forms {
 			public static Font title;
 			public static Font inputFields;
 			public static Font button;
 			public static Font link; 
 			
 			public static void setUp(JFrame window) {
-				title = new Font("Tahoma", Font.BOLD, window.getWidth()/10);
-				inputFields = new Font("Tahoma", Font.ITALIC, window.getWidth()/20);
-				button = new Font("Tahoma", Font.BOLD, window.getWidth()/25);
-				link = new Font("Tahoma", Font.ITALIC, window.getWidth()/30);
+				if( title == null ) {
+					title = new Font("Tahoma", Font.BOLD, window.getWidth()/10);
+					inputFields = new Font("Tahoma", Font.ITALIC, window.getWidth()/20);
+					button = new Font("Tahoma", Font.BOLD, window.getWidth()/25);
+					link = new Font("Tahoma", Font.ITALIC, window.getWidth()/30);
+				}
 			}
 		}
 		
@@ -45,6 +34,7 @@ public class Resources {
 	}
 	
 	public static class Colors {
+		public static Color red = new Color(194, 24, 7);
 		public static Color orange = new Color(253, 106, 2);
 		public static Color white = new Color(255, 255, 255);
 		public static Color green = new Color(41, 171, 135);
