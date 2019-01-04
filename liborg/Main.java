4464eb170227b10c29f6637b04dfc2b5246ab2d3
nooro <georgi.dancheff@gmail.com>
2019-01-04 22:54:08 +0200

    Now the entering point for the program is the log-in form

--- liborg/Main.java
+++ liborg/Main.java
@@ -1,21 +1,12 @@
 package liborg;
-import java.util.List;
 
-import dbConnection.DBConnector;
-import ui.LibraryOrganizer;
 import ui.LogInForm;
 
 public class Main {
 	
 	public static void main( String[] args) {
 
-		LibraryOrganizer liborg = new LibraryOrganizer();
-		
-//		List<Book> books = db.search("terry ");
-//		
-//		for(Book book : books) {
-//			System.out.println(book.getAutor());
-//		}
+		LogInForm login = new LogInForm();
 		
 	}
 	
