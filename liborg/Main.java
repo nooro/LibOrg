d92d5fbf38d2487263d291f5b684db373171de96
nooro <georgi.dancheff@gmail.com>
2019-01-08 18:06:08 +0200

    Just tested something

--- liborg/Main.java
+++ liborg/Main.java
@@ -1,13 +1,14 @@
 package liborg;
 
+import dbConnection.DBConnector;
 import ui.LogInForm;
 
 public class Main {
 	
 	public static void main( String[] args) {
 
-		LogInForm login = new LogInForm();
-		
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/library", "root", "");
+		LogInForm login = new LogInForm(db);
 	}
 	
 }
