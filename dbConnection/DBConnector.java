868ec5e2023a3f47f7e698bfd4618f1c2817d9b1
nooro <georgi.dancheff@gmail.com>
2019-01-08 18:04:06 +0200

    Remove the users-related and books-related db operations in separate classes

--- dbConnection/DBConnector.java
+++ dbConnection/DBConnector.java
@@ -43,6 +43,7 @@ public class DBConnector {
 	}
 	
 	public boolean isConnected() { return this.isConnected; }
+	public Connection getConnection () { return this.connection; }
 	
 	public void closeConnection() { 
 		try {
@@ -50,12 +51,11 @@ public class DBConnector {
 		} catch (SQLException e) {}
 	}
 	
-	//SEARCH.......................................
+	
 	public List<Book> search(String searchString) {
 		Statement statement;
 		String [] splittedSearchString = searchString.split(" ");
 		
-		//Build the query 
 		String query = "SELECT * FROM books";
 		if(splittedSearchString.length >= 2) {
 			for(int i = 0; i < splittedSearchString.length; i++) {
@@ -87,129 +87,4 @@ public class DBConnector {
 			return null;
 		}
 	}
-	
-	//LOG IN....................................................
-	public Result logIn(String username, String password) {
-		if(!this.isConnected) {
-			return Result.NO_SERVER_CONNECTION;
-		}
-
-		try {
-			Statement statement = connection.createStatement();
-			String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "';";
-			ResultSet result = statement.executeQuery(query);
-			
-			//There is user with that username and password
-			if( result.next() ) {
-				User.logIn(result.getInt("id"), result.getString("username"), result.getString("email"), result.getBoolean("is_admin"));
-				return Result.LOGGED_IN;
-			} else {
-				User.logOut();
-				return Result.LOG_IN_INVALID_DATA;
-			}
-			
-		} catch (SQLException e) {
-			return Result.NO_SERVER_CONNECTION;
-		}
-	}
-	
-	//REGISTRATION.....................................................................
-	public Result register(String username, String password, String email) {
-		if(!this.isConnected) {
-			return Result.NO_SERVER_CONNECTION;
-		}
-		 
-		try {
-			Statement statement = connection.createStatement();
-			String query = "SELECT id FROM users WHERE username = '" + username + "';";
-			ResultSet result = statement.executeQuery(query);
-			
-			//There is user with that username already
-			if( result.next() ) {
-				return Result.REGISTER_USERNAME_TAKEN;
-			} 
-			
-			query = "INSERT INTO `users` (`id`, `username`, `password`, `email`, `is_admin`) VALUES (NULL, '"+username+"', '"+password+"', '"+email+"', '0');";
-			statement.executeUpdate(query);
-			
-			//Check if the user is actually registered
-			query = "SELECT id FROM users WHERE username = '" + username + "';";
-			result = statement.executeQuery(query);
-			if( result.next() ) {
-				User.logIn(result.getInt("id"), username, email, false);
-				return Result.REGISTERED;
-			} 
-			else return Result.SQL_ERROR;
-			
-		} catch (SQLException e) {
-			return Result.NO_SERVER_CONNECTION;
-		}
-	}
-	
-	//ADD NEW BOOK..................................................................................................
-	public Result addNewBook(String title, String author, String genre, int publishingYear, int pages, int amount) {
-		if(!this.isConnected) {
-			return Result.NO_SERVER_CONNECTION;
-		}
-		
-		try {
-			Statement statement = connection.createStatement();
-			String query = "SELECT id, amount FROM books WHERE title = '" + title + "' AND author = '"+author+"';";
-			ResultSet result = statement.executeQuery(query);
-			
-			//If this book is already in the system just increment the amount with the new amount
-			if(result.next()) {
-				query = "UPDATE `books` SET `amount` = '"+(result.getInt("amount")+amount)+"' WHERE `books`.`id` = " + result.getInt("id") + ";";
-				statement.executeUpdate(query);
-				return Result.AMOUNT_IS_CHANGED;
-			}
-			else { //add the new book
-				query = "INSERT INTO `books` (`id`, `title`, `author`, `genre`, `publishing_year`, `pages`, `amount`) VALUES (NULL, '"+title+"', '"+author+"', '"+genre+"', '"+publishingYear+"', '"+pages+"', '"+amount+"');";
-				statement.executeUpdate(query);
-				
-				//Check if the book is added
-				query = "SELECT id FROM books WHERE title = '" + title + "' AND author = '"+author+"';";
-				result = statement.executeQuery(query);
-				if( result.next() ) {
-					return Result.NEW_BOOK_IS_ADDED;
-				} else return Result.SQL_ERROR;
-			}
-			
-		}catch(SQLException e) {
-			return Result.NO_SERVER_CONNECTION;
-		}
-	}
-	
-	//REMOVE BOOK.....................
-	public Result removeBook(int id) {
-		if(!this.isConnected) {
-			return Result.NO_SERVER_CONNECTION;
-		}
-		
-		try {
-			Statement statement = connection.createStatement();
-			String query = "SELECT amount FROM books WHERE id = '" + id + "';";
-			ResultSet result = statement.executeQuery(query);
-			
-			//Check if there is book with the given id
-			if(result.next()) {
-				//If this is the only book remove it
-				if(result.getInt("amount") == 1) {
-					query = "DELETE FROM `books` WHERE `books`.`id` = " + id + ";";
-					statement.executeUpdate(query);
-					return Result.BOOK_IS_REMOVED;
-				}
-				else { //Take one from the current amount
-					query = "UPDATE `books` SET `amount` = '"+(result.getInt("amount")-1)+"' WHERE `books`.`id` = " + id + ";";
-					statement.executeUpdate(query);
-					return Result.AMOUNT_IS_CHANGED;
-				}
-			}
-			else return Result.INVALID_BOOK_ID;
-			
-		}catch(SQLException e) {
-			return Result.SQL_ERROR;
-		}
-	}
-
 }
