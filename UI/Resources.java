dc1faf607917f7dce645acd28ce65278f58cd51f
nooro <georgi.dancheff@gmail.com>
2019-01-08 04:17:13 +0200

    Update for the main window

--- UI/Resources.java
+++ UI/Resources.java
@@ -9,6 +9,7 @@ public class Resources {
 	
 	public static class Fonts {
 		
+		//Fonts for the log-in and registration forms
 		public static class Forms {
 			public static Font title;
 			public static Font inputFields;
@@ -25,9 +26,16 @@ public class Resources {
 			}
 		}
 		
+		//Fonts for the main program
 		public static class LibraryOrganizer {
+			public static Font logOut;
+			public static Font buttons;
+			
 			public static void setUp(JFrame window) {
-				
+				if(logOut == null) {
+					logOut = new Font("Tahoma", Font.ITALIC, window.getWidth()/100);
+					buttons = new Font("Tahoma", Font.BOLD, window.getWidth()/105);
+				}
 			}
 		}
 		
@@ -37,7 +45,9 @@ public class Resources {
 		public static Color red = new Color(194, 24, 7);
 		public static Color orange = new Color(253, 106, 2);
 		public static Color white = new Color(255, 255, 255);
+		public static Color black = new Color(0, 0, 0);
 		public static Color green = new Color(41, 171, 135);
 		public static Color background = new Color(44, 62, 80);
+		public static Color headerBackground = new Color(49, 51, 53);
 	}
 }
