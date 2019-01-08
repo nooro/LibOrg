1405369978f1612025a6f124de1ddf19ef4a72af
nooro <georgi.dancheff@gmail.com>
2019-01-08 04:20:43 +0200

    The DB connector results are now in one enum. Added options to add new book and delete a book

--- dbConnection/DBConnector.java
+++ dbConnection/DBConnector.java
@@ -25,6 +25,23 @@ public class DBConnector {
 		}
 	}
 	
+	public enum Result {
+		NO_SERVER_CONNECTION,
+		SQL_ERROR,
+
+		LOG_IN_INVALID_DATA,
+		LOGGED_IN,
+		
+		REGISTER_USERNAME_TAKEN,
+		REGISTERED,
+		
+		NEW_BOOK_IS_ADDED,
+		AMOUNT_IS_CHANGED,
+		
+		BOOK_IS_REMOVED,
+		INVALID_BOOK_ID
+	}
+	
 	public boolean isConnected() { return this.isConnected; }
 	
 	public void closeConnection() { 
@@ -72,15 +89,9 @@ public class DBConnector {
 	}
 	
 	//LOG IN....................................................
-	public enum LogInResult {
-		NO_SERVER_CONNECTION,
-		INVALID_DATA,
-		LOGGED_IN
-	}
-	
-	public LogInResult logIn(String username, String password) {
+	public Result logIn(String username, String password) {
 		if(!this.isConnected) {
-			return LogInResult.NO_SERVER_CONNECTION;
+			return Result.NO_SERVER_CONNECTION;
 		}
 
 		try {
@@ -90,29 +101,22 @@ public class DBConnector {
 			
 			//There is user with that username and password
 			if( result.next() ) {
-				User.logIn(result.getInt("id"), result.getString("username"), result.getString("email"));
-				return LogInResult.LOGGED_IN;
+				User.logIn(result.getInt("id"), result.getString("username"), result.getString("email"), result.getBoolean("is_admin"));
+				return Result.LOGGED_IN;
 			} else {
 				User.logOut();
-				return LogInResult.INVALID_DATA;
+				return Result.LOG_IN_INVALID_DATA;
 			}
 			
 		} catch (SQLException e) {
-			return LogInResult.NO_SERVER_CONNECTION;
+			return Result.NO_SERVER_CONNECTION;
 		}
 	}
 	
-	//REGISTRATION........................................................
-	public enum RegistrationResult {
-		NO_SERVER_CONNECTION,
-		USERNAME_TAKEN,
-		REGISTERED,
-		SQL_ERROR
-	}
-	
-	public RegistrationResult register(String username, String password, String email) {
+	//REGISTRATION.....................................................................
+	public Result register(String username, String password, String email) {
 		if(!this.isConnected) {
-			return RegistrationResult.NO_SERVER_CONNECTION;
+			return Result.NO_SERVER_CONNECTION;
 		}
 		 
 		try {
@@ -122,23 +126,90 @@ public class DBConnector {
 			
 			//There is user with that username already
 			if( result.next() ) {
-				return RegistrationResult.USERNAME_TAKEN;
+				return Result.REGISTER_USERNAME_TAKEN;
 			} 
 			
-			query = "INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL, '"+username+"', '"+password+"', '"+email+"');";
+			query = "INSERT INTO `users` (`id`, `username`, `password`, `email`, `is_admin`) VALUES (NULL, '"+username+"', '"+password+"', '"+email+"', '0');";
 			statement.executeUpdate(query);
 			
 			//Check if the user is actually registered
 			query = "SELECT id FROM users WHERE username = '" + username + "';";
 			result = statement.executeQuery(query);
 			if( result.next() ) {
-				User.logIn(result.getInt("id"), username, email);
-				return RegistrationResult.REGISTERED;
+				User.logIn(result.getInt("id"), username, email, false);
+				return Result.REGISTERED;
 			} 
-			else return RegistrationResult.SQL_ERROR;
+			else return Result.SQL_ERROR;
 			
 		} catch (SQLException e) {
-			return RegistrationResult.NO_SERVER_CONNECTION;
+			return Result.NO_SERVER_CONNECTION;
 		}
 	}
+	
+	//ADD NEW BOOK..................................................................................................
+	public Result addNewBook(String title, String author, String genre, int publishingYear, int pages, int amount) {
+		if(!this.isConnected) {
+			return Result.NO_SERVER_CONNECTION;
+		}
+		
+		try {
+			Statement statement = connection.createStatement();
+			String query = "SELECT id, amount FROM books WHERE title = '" + title + "' AND author = '"+author+"';";
+			ResultSet result = statement.executeQuery(query);
+			
+			//If this book is already in the system just increment the amount with the new amount
+			if(result.next()) {
+				query = "UPDATE `books` SET `amount` = '"+(result.getInt("amount")+amount)+"' WHERE `books`.`id` = " + result.getInt("id") + ";";
+				statement.executeUpdate(query);
+				return Result.AMOUNT_IS_CHANGED;
+			}
+			else { //add the new book
+				query = "INSERT INTO `books` (`id`, `title`, `author`, `genre`, `publishing_year`, `pages`, `amount`) VALUES (NULL, '"+title+"', '"+author+"', '"+genre+"', '"+publishingYear+"', '"+pages+"', '"+amount+"');";
+				statement.executeUpdate(query);
+				
+				//Check if the book is added
+				query = "SELECT id FROM books WHERE title = '" + title + "' AND author = '"+author+"';";
+				result = statement.executeQuery(query);
+				if( result.next() ) {
+					return Result.NEW_BOOK_IS_ADDED;
+				} else return Result.SQL_ERROR;
+			}
+			
+		}catch(SQLException e) {
+			return Result.NO_SERVER_CONNECTION;
+		}
+	}
+	
+	//REMOVE BOOK.....................
+	public Result removeBook(int id) {
+		if(!this.isConnected) {
+			return Result.NO_SERVER_CONNECTION;
+		}
+		
+		try {
+			Statement statement = connection.createStatement();
+			String query = "SELECT amount FROM books WHERE id = '" + id + "';";
+			ResultSet result = statement.executeQuery(query);
+			
+			//Check if there is book with the given id
+			if(result.next()) {
+				//If this is the only book remove it
+				if(result.getInt("amount") == 1) {
+					query = "DELETE FROM `books` WHERE `books`.`id` = " + id + ";";
+					statement.executeUpdate(query);
+					return Result.BOOK_IS_REMOVED;
+				}
+				else { //Take one from the current amount
+					query = "UPDATE `books` SET `amount` = '"+(result.getInt("amount")-1)+"' WHERE `books`.`id` = " + id + ";";
+					statement.executeUpdate(query);
+					return Result.AMOUNT_IS_CHANGED;
+				}
+			}
+			else return Result.INVALID_BOOK_ID;
+			
+		}catch(SQLException e) {
+			return Result.SQL_ERROR;
+		}
+	}
+
 }
